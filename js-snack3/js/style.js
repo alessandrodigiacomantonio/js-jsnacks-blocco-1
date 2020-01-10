3f17700ee03348a33357ea6362d51e4238feaa9a
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-10 17:19:38 +0100

    js-snack3: miglioria

--- js-snack3/js/style.js
+++ js-snack3/js/style.js
@@ -3,9 +3,8 @@ var somma = 0;
 //-----
 //con ciclo for
 //-----
-// for (var i = 0; i < 5; i++) {
-//   somma += parseInt(prompt('Inserisci il ' + ++i + '° numero'));
-//   i--;
+// for (var i = 1; i < 5; i++) {
+//   somma += parseInt(prompt('Inserisci il ' + i + '° numero'));
 //   console.log(somma);
 // }
 // alert('la somma dei numeri inseriti è: ' + somma);
