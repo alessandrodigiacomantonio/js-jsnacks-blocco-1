281f71c6ea199a1b2db7d9a4a305a0c35dcee8f6
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-10 16:25:09 +0100

    js-snack3

---
+++ js-snack3/js/style.js
@@ -0,0 +1,21 @@
+var somma = 0;
+
+//-----
+//con ciclo for
+//-----
+// for (var i = 0; i < 5; i++) {
+//   somma += parseInt(prompt('Inserisci il ' + ++i + '° numero'));
+//   i--;
+//   console.log(somma);
+// }
+// alert('la somma dei numeri inseriti è: ' + somma);
+
+//-----
+//con ciclo while
+//-----
+var i = 0;
+while (i < 5) {
+  somma += parseInt(prompt('Inserisci il ' + ++i + '° numero'));
+  console.log(somma);
+}
+alert('la somma dei numeri inseriti è: ' + somma);
