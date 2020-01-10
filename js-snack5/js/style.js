6ade173ecf406713d3295db5d31c79e4c820e37e
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-10 16:56:02 +0100

    js-snack5

---
+++ js-snack5/js/style.js
@@ -0,0 +1,32 @@
+var dispari = [];
+var numeri;
+var carlo='intero positivo';
+//----------
+//con ciclo for
+//---------
+for (var i = 0; i < 6; i++) {
+  numeri = parseInt(prompt('Inserisci il '+ ++i +'° numero', carlo));
+  console.log(numeri);
+  i--;
+  if (numeri % 2 != 0) {
+    dispari.push(numeri);
+  }
+}
+console.log(dispari);
+alert(dispari);
+
+//----------
+//con ciclo while
+//----------
+// var j= 0;
+// while (j < 6) {
+//   numeri = parseInt(prompt('Inserisci il '+ ++j +'° numero', carlo));
+//   console.log(numeri);
+//   j--;
+//   if (numeri % 2 != 0) {
+//     dispari.push(numeri);
+//   }
+//   j++;
+// }
+// console.log(dispari);
+// alert(dispari);
